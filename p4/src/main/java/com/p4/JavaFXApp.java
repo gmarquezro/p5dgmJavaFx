445e5500a731ba69a87a4b79b9999b613b03725f
gmarquezro <73852571+gmarquezro@users.noreply.github.com>
2021-05-22 12:49:02 +0200

    Add files via upload
    first commit

---
+++ p4/src/main/java/com/p4/JavaFXApp.java
@@ -0,0 +1,13 @@
+package com.p4;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javafx.application.Application;
+
+@SpringBootApplication
+public class JavaFXApp{
+
+	public static void main(String[] args) {
+		Application.launch(P4Application.class, args);	
+		}
+}
