d6d55b2ce55ee145be57137667647cd91993ab5f
MCarmen72 <mreinah@uoc.edu>
2021-06-03 00:21:08 +0200

    Socio finalizado

---
+++ p4/src/main/java/com/repositories/SocioRepository.java
@@ -0,0 +1,9 @@
+package com.repositories;
+
+import com.entities.Socio;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SocioRepository extends CrudRepository<Socio, Integer>{
+
+}
